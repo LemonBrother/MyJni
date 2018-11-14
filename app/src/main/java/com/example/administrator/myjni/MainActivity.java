70283fae6be082c76d75deb9b48c29fec2539219
Administrator <254009380@qq.com>
2018-11-14 11:27:09 +0800

    emm

--- app/src/main/java/com/example/administrator/myjni/MainActivity.java
+++ app/src/main/java/com/example/administrator/myjni/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //ssdsdsd
 
     // Example of a call to a native method
     TextView tv = (TextView) findViewById(R.id.sample_text);
