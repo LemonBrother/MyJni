bdd94816065e63ce86a881d05287fa64296c16c8
Administrator <254009380@qq.com>
2018-11-14 11:02:27 +0800

    emm

---
+++ app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,159 @@
+#include <jni.h>
+#include <string>
+#include <string.h>
+#include <stdio.h>
+
+
+
+
+#include <stdlib.h>
+#include <limits.h>
+#include <stdio.h>
+#include <errno.h>
+
+
+extern "C"
+JNIEXPORT jstring JNICALL
+Java_com_example_administrator_myjni_MainActivity_stringFromJNI(
+        JNIEnv* env,
+        jobject /* this */) {
+    std::string hello = "Hello from C++";
+    return env->NewStringUTF(hello.c_str());
+}
+
+extern "C"
+JNIEXPORT jstring JNICALL
+Java_com_example_administrator_myjni_MainActivity_FromatstringFromJNI(
+        JNIEnv* env,jobject obj,jstring phoneid) {
+
+
+std::string hello = "Hello from C++";
+//return env->NewStringUTF(hello.c_str());
+
+//    return env->NewStringUTF(hello.substr(0,3).c_str());
+
+
+
+    //指向常量的指针
+      const  char*  str  = env->GetStringUTFChars(phoneid,0);
+
+
+
+
+
+
+    //声明三个临时字符串
+    char aa[3] =""  ;
+    char bb[3] =""  ;
+    char cc[2] =""  ;
+
+    //拆成3部分分别进行赋值
+    strncpy(aa,str,3);          //从第三位开始截取
+    strncpy(bb,str+3,3);
+    strncpy(cc,str+6,2);
+
+
+
+
+   // temp=strchr(str,5);
+
+   // strcat(temp,str);
+
+
+
+//     char  buff[128] = env->GetStringUTFChars(phoneid,0);
+//    char* pBuff = buff  ;
+
+
+  //  strlcpy(aa,str,3);    //可以复制
+
+
+
+
+
+
+    //三句一起执行，闪退提示lock
+//  env-> GetStringUTFRegion(phoneid,0,3,aa);
+//    env-> GetStringUTFRegion(phoneid,3,6,bb);
+//    env-> GetStringUTFRegion(phoneid,6,8,cc);
+
+
+   // env->ReleaseStringChars(phoneid,chr1);
+
+    int a = atoi(aa)*3;
+    int b = atoi(bb)*6;
+    int c = atoi(cc)*8;
+
+
+    char *s1 = new  char();
+    char *s2 = new  char();
+    char *s3 = new  char();
+    sprintf(s1,"%d",a);
+        sprintf(s2,"%d",b);
+    sprintf(s3,"%d",c);
+
+//    char *string1 = (char *) malloc(sizeof(char) * 16);//c中的写法，c++中可以直接new
+//    char *string2 = (char *) malloc(sizeof(char) * 16);
+//    char *string3 = (char *) malloc(sizeof(char) * 16);
+//    sprintf(string1,"%d",a);
+//    sprintf(string2,"%d",b);
+//    sprintf(string3,"%d",c);
+
+
+    strcat(s3,s2);
+    strcat(s3,s1);
+    //加密算法///////////////////////////////////////
+
+
+    //获取长度，用于截取
+    int len = strlen(s3);
+
+
+
+    char x[20] = "";
+    char  y[8] = "";
+
+     strncpy(x,s3,8);
+     strncpy(y,s3+8,len-8);
+
+    int yi = (atoi(y)*35)+100;
+
+
+
+    char *ys=(char *) malloc(sizeof(char) * 16);;
+    sprintf(ys,"%d",yi);
+
+    int yslen = strlen(ys);
+
+    strncpy(y ,ys+(yslen-2),2);
+
+    strcat(x,y);
+
+    delete s1;
+    delete s2;
+    delete s3;
+       env->ReleaseStringUTFChars(phoneid,str);
+
+
+
+
+
+    return env->NewStringUTF( x);
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ app/src/main/java/com/example/administrator/myjni/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.administrator.myjni;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    // Example of a call to a native method
+    TextView tv = (TextView) findViewById(R.id.sample_text);
+    tv.setText(stringFromJNI());
+
+        tv.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,FromatstringFromJNI( "19095857" ),Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+    public native String FromatstringFromJNI(String phoneid);
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+}
